b41454dc008536289bd9b740a42faa277ebd151d
maciej-stanek <makaron28@gmail.com>
2017-07-11 21:40:47 +0200

    wip parser

--- Parser/chord2png.sh
+++ Parser/chord2png.sh
@@ -10,6 +10,9 @@ if [[ ! $2 =~ [0-9]{4} ]]; then
 	exit 1
 fi
 
+CHORDS_DIR=chords
+mkdir -p $CHORDS_DIR
+
 CHORD_NAME=$1
 CHORD=(${2:0:1} ${2:1:1} ${2:2:1} ${2:3:1})
 echo "Got chord name \"$CHORD_NAME\" and strings (${CHORD[@]})"
@@ -20,32 +23,48 @@ for i in ${CHORD[@]}; do
 		STRING_MAX=$i
 	fi
 done
-echo "Max string value is $STRING_MAX"
+echo "Max bar distance is $STRING_MAX"
 
 IM_CMD="convert"
-IM_POINTSIZE=70
-IM_WIDTH=200
-IM_HEIGHT=300
+IM_WIDTH=400
+IM_SIDEMARGIN=10
+IM_POINTSIZE=140
+IM_BARSPACING=90
+IM_BAROVERFLOW=30
+IM_NOTERADIUS=30
 IM_TOPMARGIN=$((IM_POINTSIZE+15))
-IM_BOTTOMEND=$((IM_HEIGHT-10))
+IM_BOTTOMMARGIN=10
+IM_BARTOPTHICKNESS=16
+IM_BARTHICKNESS=10
+IM_STRINGTHICKNESS=6
+IM_BARCOUNT=$((STRING_MAX>4?STRING_MAX:4))
+IM_HEIGHT=$((IM_TOPMARGIN+IM_BARCOUNT*IM_BARSPACING+IM_BOTTOMMARGIN))
+echo "IM_HEIGHT=$IM_HEIGHT"
 IM_STRINGS_D=($((IM_WIDTH*1/8)) $((IM_WIDTH*3/8)) $((IM_WIDTH*5/8)) $((IM_WIDTH*7/8)))
-IM_STRINGS_OPTS=
-for d in ${IM_STRINGS_D[@]}; do
-	IM_STRINGS_OPTS+=" -draw 'stroke-linecap round line $d,$IM_TOPMARGIN $d,$IM_BOTTOMEND'"
-done
 
 IM_CMD+=" -size ${IM_WIDTH}x${IM_HEIGHT} xc:white"
 IM_CMD+=" -fill black"
 IM_CMD+=" -gravity north"
 IM_CMD+=" -pointsize $IM_POINTSIZE"
-IM_CMD+=" -font DejaVu-Serif-Condensed-Italic"
+IM_CMD+=" -font Ubuntu-Condensed"
 IM_CMD+=" -draw 'text 0,0 $1'"
-IM_CMD+=" -strokewidth 2"
 IM_CMD+=" -stroke black"
-IM_CMD+=$IM_STRINGS_OPTS
-IM_CMD+=" -strokewidth 8"
-IM_CMD+=" -draw 'stroke-linecap round line 10,$IM_TOPMARGIN 190,$IM_TOPMARGIN'"
-IM_CMD+=" $1.png"
+IM_CMD+=" -strokewidth $IM_BARTHICKNESS"
+for (( i=1; i<$IM_BARCOUNT; i++ )); do
+	topd=$((IM_TOPMARGIN+IM_BARSPACING*i))	
+	IM_CMD+=" -draw 'line $((IM_STRINGS_D[0]-IM_BAROVERFLOW)),$topd $((IM_STRINGS_D[3]+IM_BAROVERFLOW)),$topd'"
+done
+IM_CMD+=" -strokewidth $IM_STRINGTHICKNESS"
+for i in 0 1 2 3; do
+	IM_CMD+=" -draw 'line ${IM_STRINGS_D[$i]},$IM_TOPMARGIN ${IM_STRINGS_D[$i]},$((IM_HEIGHT-IM_BOTTOMMARGIN))'"
+	if [[ ${CHORD[$i]} > 0 ]]; then
+		topd=$(((CHORD[$i]-1)*IM_BARSPACING+IM_BARSPACING/2+IM_TOPMARGIN))
+		IM_CMD+=" -draw 'circle ${IM_STRINGS_D[$i]},$topd,$((IM_STRINGS_D[$i]+IM_NOTERADIUS)),$topd'"
+	fi
+done
+IM_CMD+=" -strokewidth $IM_BARTOPTHICKNESS"
+IM_CMD+=" -draw 'line $IM_SIDEMARGIN,$IM_TOPMARGIN $((IM_WIDTH-IM_SIDEMARGIN)),$IM_TOPMARGIN'"
+IM_CMD+=" $CHORDS_DIR/$1.png"
 
 echo $IM_CMD
 eval $IM_CMD
